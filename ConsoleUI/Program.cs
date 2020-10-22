7a2d8d444f0c6951493cde941a137c2c0b2abc9c
RayMoroney <72605559+RayMoroney@users.noreply.github.com>
2020-10-22 17:13:42 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using DemoLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IManager accountingVP = new Manager();
+
+            accountingVP.FirstName = "Mary";
+            accountingVP.LastName = "Daley";
+            accountingVP.CalculatePerHourRate(4);
+
+            IManaged emp = new Manager();
+
+            emp.FirstName = "John";
+            emp.LastName = "Doe";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+
+
+            Console.ReadLine();
+        }
+    }
+}
